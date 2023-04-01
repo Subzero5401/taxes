bee8585ce47499a7c12b262867f425f117db83ee
Dustin Carpenter <88062861+Subzero5401@users.noreply.github.com>
2023-03-31 19:41:59 -0500

    Update Source.cpp
    Added commits . Change variable for readability and clarity.

--- Source.cpp
+++ Source.cpp
@@ -6,78 +6,78 @@ using namespace std;
 int main()
 {
 	double socialmaxtax = 160200;
-	double bracketper10 = 10275;
-	double bracketper12 = 41775;
-	double bracketper22 = 89075;
-	double bracketper24 = 170050;
-	double bracketper32 = 215950;
-	double bracketper35 = 539900;
-	double bracketper37 = bracketper35 + 1;
-	double step12per = bracketper10 * .1;
-	double step22per = (bracketper12 - bracketper10) * .12 + step12per;
-	double step24per = (bracketper22 - bracketper12) * .22 + step22per;
-	double step32per = (bracketper24 - bracketper22) * .24 + step24per;
-	double step35per = (bracketper32 - bracketper24) * .32 + step32per;
-	double step37per = (bracketper35 - bracketper32) * .35 + step35per;
+	double bracketper10 = 10275;							  //Ten percentage bracket maximum threshold
+	double bracketper12 = 41775;							  //12 Percentage bracket max threshold
+	double bracketper22 = 89075;							  //22 percentage bracket maX threshold
+	double bracketper24 = 170050;							  //24 percentage bracket maX threshold
+	double bracketper32 = 215950;							  //32 percentage bracket maX threshold
+	double bracketper35 = 539900;							  //35 percentage bracket maX threshold
+	double bracketper37 = bracketper35 + 1;						  //37 percentage bracket maX threshold
+	double step12per = bracketper10 * .1;                                     	  //tax amount carried into the 12 % bracket 
+	double step22per = (bracketper12 - bracketper10) * .12 + step12per;		  // " amount carried into the 22 % bracket
+	double step24per = (bracketper22 - bracketper12) * .22 + step22per;		  // " amount carried into the 24 % bracket
+	double step32per = (bracketper24 - bracketper22) * .24 + step24per;		  // " amount carried into the 32 % bracket
+	double step35per = (bracketper32 - bracketper24) * .32 + step32per;		  // " amount carried into the 35 % bracket
+	double step37per = (bracketper35 - bracketper32) * .35 + step35per;		  // " amount carried into the 37 % bracket
 
 
 
 
-	double b = 0; //Intialize for salary 
-	int loop = 1;
+	double salary = 0; //Initialize for salary to zero
+	int loop = 1; // used to loop
 	double incomewith;
-	int a = 1;
-	while (a == 1) {
+	
+	while (loop == 1) {
 		cout << " Enter your salary ";
 
-		cin >> b;
-		if (0 >= b)
+		cin >> salary;
+		if (0 >= salary)
 		{
 			incomewith = 0;
 
 			cout << "Make some money you lazy bum :)";
 		}
-		else if (b <= bracketper10)
+		else if (salary <= bracketper10)
 		{
-			incomewith = b * .10;
+			incomewith = salary * .10;
 
 		}
-		else if (b <= bracketper12)
+		else if (salary <= bracketper12)
 		{
 
-			incomewith = (b - bracketper10) * .12 + step12per;
+			incomewith = (salary - bracketper10) * .12 + step12per;
 
 		}
-		else if (b <= bracketper22)
+		else if (salary <= bracketper22)
 		{
-			incomewith = (b - bracketper12) * .22 + step22per ;
-
-
-
-		}
-		else if (b <= bracketper24)
-		{
-			incomewith = (b - bracketper22) * .24 + step24per;
-
+			incomewith = (salary - bracketper12) * .22 + step22per;
 
 
 
 		}
-		else if (b <= bracketper32)
+		else if (salary <= bracketper24)
 		{
-			incomewith = (b - bracketper24) * .32 + step32per;
+			incomewith = (salary - bracketper22) * .24 + step24per;
+
+
 
 
 		}
-		else if (b <= bracketper35)
+		else if (salary <= bracketper32)
 		{
+			incomewith = (salary - bracketper24) * .32 + step32per;
 
-			incomewith = (b - bracketper32) * .35 + step35per;
 
 		}
-		else if (b >= bracketper37)
+		else if (salary <= bracketper35)
 		{
-			incomewith = (b - bracketper35) * .37 + step37per;
+
+			incomewith = (salary - bracketper32) * .35 + step35per;
+
+		}
+		else if (salary >= bracketper37)
+		{
+			incomewith = (salary - bracketper35) * .37 + step37per;
 
 		}
 		else
@@ -88,28 +88,28 @@ int main()
 		}
 
 		double socialtax;
-		if (b <= socialmaxtax)
+		if (salary <= socialmaxtax)
 		{
-			socialtax = b * .062;
+			socialtax = salary * .062;
 
 		}
-		else if (b > socialmaxtax)
+		else if (salary > socialmaxtax)
 		{
 			socialtax = socialmaxtax * .062;
 
 		}
 
-		double medicadetax = b * .0145;
+		double medicadetax = salary * .0145;
 
 
 
-		cout << " " << b << " your total salary " << endl;
+		cout << " " << salary << " your total salary " << endl;
 		cout << " Federal income withheld is " << incomewith << endl;
 		cout << " social security withheld is " << socialtax << endl;
 		cout << " medicaid withheld " << medicadetax << endl << " ------------------------------" << endl;
 		cout << " how much withheld total " << incomewith + socialtax + medicadetax << endl;
-		cout << " your total payout average per week after taxes " << (b - incomewith - socialtax - medicadetax) / 52 << endl;
+		cout << " your total payout average per week after taxes " << (salary - incomewith - socialtax - medicadetax) / 52 << endl;
 		cout << " do you wish to start again 1 for yes , 0 for no " << endl;
-		cin >> a;
+		cin >> loop;
 	}
 }
